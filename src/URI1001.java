9c32c93b2827c1e6fbb8853a06ee4e262e7a8f74
JheannyC <jeane.amarela@gmail.com>
2018-08-25 18:12:24 -0300

    Initial commit

---
+++ src/URI1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class URI1001 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int A = Integer.parseInt(input.nextLine());
+        int B = Integer.parseInt(input.nextLine());
+        int x = A+B;
+        System.out.println("X = "+x);
+
+    }
+}
